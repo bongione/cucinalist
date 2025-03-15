4c4dcab0ee48b8dd2d6723a45a0ac3dc9a73d704
Paolo Ferdinando Bongiovanni <paolo.bongiovanni@mindiply.com>
2025-03-15 19:02:17 +0000

    Removed non-source folder

--- packages/graphql/app/vitest.config.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
-export default defineConfig({
-    test: {
-        globals: true, // Enables `describe`, `it`, and `expect` globally
-        environment: "node", // Ensures it runs in a Node.js environment
-    },
-});
